851e44edf4269b4c161a463df1791315e6d8ea74
diyorbekMIT <diyorbekjon2202@gmail.com>
2025-04-11 23:02:43 +0900

    feat: getRestaurant

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -187,5 +187,18 @@ memberController.getTopUsers = async (req: ExtendedRequest, res: Response) => {
   }
 }
 
+memberController.getRestaurant = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("getRestaurant");
+    const result = await memberService.getRestaurant();
+
+    res.status(HttpCode.OK).json(result);
+  } catch(err) {
+    console.log("ERROR on getRestaurant", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard)
+  }
+}
+
 
 export default memberController;
--- src/models/Member.Service.ts
+++ src/models/Member.Service.ts
@@ -142,6 +142,14 @@ class MemberService {
 
         return result;
       }
+
+      public async getRestaurant(): Promise<Member> {
+        const result = await this.memberModel.findOne({memberType: MemberType.RESTAURANT}).lean().exec()
+
+        if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+        return result;
+      }
 }
 
 export default MemberService;
